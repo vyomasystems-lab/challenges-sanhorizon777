4b3f55d8fd7fb6685152d1c69880ff401cd833b4
sanhorizon777 <109404741+sanhorizon777@users.noreply.github.com>
2022-07-27 19:13:29 +0530

    Update test_mkbitmanip.py

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -34,10 +34,13 @@ def run_test(dut):
 
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
-    mav_putvalue_src1 = 0x5
-    mav_putvalue_src2 = 0x0
-    mav_putvalue_src3 = 0x0
-    mav_putvalue_instr = 0x101010B3
+    mav_putvalue_src1 = 0x0     # Initializing source 1 input
+    mav_putvalue_src2 = 0x0     # Initializing source 2 input
+    mav_putvalue_src3 = 0x0     # Initializing source 3 input
+    mav_putvalue_instr = 0x401070B3        # hexadecimal Instruction code for ANDN1
+    for mav_putvalue_src3 in range(0,4294967296,1):                # Running a nested for loop comparing the values of all 3 inputs ranging from
+        for mav_putvalue_src2 in range(0,4294967296,1):            # 0x00000000 to 0xffffffff
+            for mav_putvalue_src1 in range(0,4294967296,1):
 
     # expected output from the model
     expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
@@ -56,7 +59,10 @@ def run_test(dut):
 
     cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
     cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+    cocotb.log.info(f'src1 value ={hex(mav_putvalue_src1)}')    # Checking the value of source 1 for which error is occuring
+    cocotb.log.info(f'src2 value ={hex(mav_putvalue_src2)}')    # Checking the value of source 2 for which error is occuring
+    cocotb.log.info(f'src3 value ={hex(mav_putvalue_src3)}')    # Checking the value of source 3 for which error is occuring
     
     # comparison
-    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)} for src1 values = {hex(mav_putvalue_src1)} and for src2 = {hex(mav_putvalue_src2)}'
     assert dut_output == expected_mav_putvalue, error_message
